4648600cead38f52767d103aed77131c0e5dda87
lnarote777 <nolara@iesrafaelalberti.es>
2024-01-30 19:53:31 +0100

    menú creado

--- Ejericio9/src/Main.kt
+++ Ejericio9/src/Main.kt
@@ -1,3 +1,24 @@
 fun main(){
 
+    try {
+        menu()
+        val opcion = pedirOpcion()
+
+    }
+
+}
+fun menu(){
+    println("1. Agregar tarea.")
+    println("2. Eliminar tarea.")
+    println("3. Mostrar tareas.")
+    println("4. Cambiar estado tarea.")
+    println("5. Mostrar tareas pendientes.")
+    println("6. Mostrar tareas finalizadas.")
+    println("0. Salir.")
+}
+
+fun pedirOpcion(): Int {
+    print("Seleccione una opción -> ")
+    val opcion = readln().toInt()
+    return opcion
 }
